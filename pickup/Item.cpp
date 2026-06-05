c3d8c5769e7dcc60b9493275df10449f0309b4bb
GM2150182\ydauser01 <yuki_kensyou@icloud.com>
2026-06-05 09:07:04 +0900

    Item基底クラス追加

---
+++ pickup/Item.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include "Enemy.h"
+#include "Collider.h"
+
+void Enemy::OnCollision(BaseCollider* collider)
+{
+
+}
+
+void Enemy::Initialize()
+{
+
+}
+
+void Enemy::Finalize()
+{
+
+}
+
+void Enemy::Update()
+{
+}
+
+void Enemy::Draw(const Camera& camera)
+{
+
+}
---
+++ pickup/Item.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "GameObject.h"
+class Enemy :
+    public GameObject
+{
+public:
+	void OnCollision(BaseCollider* collider) override;
+
+	void Initialize();
+	void Finalize();
+	void Update();
+	void Draw(const Camera& camera) override;
+};
+
